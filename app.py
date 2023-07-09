7c3235d157d95d661138cc83f7b23b5d9da244b9
Moein <moein.mac@gmail.com>
2023-07-09 10:19:42 +0330

    crop Img and AvgRGB

--- app.py
+++ app.py
@@ -18,3 +18,20 @@ def resizeSamples(samples, mosaicSize):
   for index in range(len(samples)):
     samples[index] = cv2.resize(
       samples[index], mosaicSize, interpolation=cv2.INTER_AREA)
+    
+def avgRGB(image):
+  (h, w, d) = image.shape
+  image = np.array(image)
+  image = image.reshape((h*w, d))
+  return np.average(image, axis=0)
+
+
+def cropImage(image, mosaicSize, mosaicCount):
+  h, w = mosaicSize
+  row, col = mosaicCount
+  croppedImgs = []
+  for j in range(col):
+    for i in range(row):
+      croppedImgs.append(image[i * w: (i + 1) * w, j * h: (j + 1) * h])
+  return (croppedImgs)
+
